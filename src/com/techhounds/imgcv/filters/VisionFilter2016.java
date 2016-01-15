e93bff47175f0fbb188efa9e5861eb8cf3e8979f
Caleb <Caleb>
2016-01-14 20:00:16 -0800

    First version of the 2016 robot vision code.
    Currently, we have the color filter working and color data collected from
    three images, though the color filter values are not implemented yet.
    
    Also, I have been unable to connect to 868-karl and capture more images,
    and 3 images certainly aren't enough to determine an accurate,
    multi-purpose color range...we may have to find some other method of
    capturing images and import them into the Viewer Tool.

---
+++ src/com/techhounds/imgcv/filters/VisionFilter2016.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2013, Paul Blankenbaker
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.techhounds.imgcv.filters;
+
+import com.techhounds.imgcv.PolygonCv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+/**
+ * A image filter which performs all of the operations in an attempt to locate
+ * the rectangular target areas from the 2013 FRC competition.
+ *
+ * @author Paul Blankenbaker
+ */
+public final class VisionFilter2016 implements MatFilter {
+
+    private final MatFilter _ColorRange; //Used to filter image to a specific color range.
+    //private final GrayScale _GrayScale; //Used to convert image to a gray scale rendering.
+    //private final BlackWhite _BlackWhite; //Used to convert from gray scale to black and white.
+
+    /**
+     * 
+     */
+    public VisionFilter2016() { //Constructs a new instance by pre-allocating all of our image filtering objects.
+    	_ColorRange = createColorRange();
+        //_GrayScale = new GrayScale();
+        //_BlackWhite = createBlackWhite();
+    }
+
+    /**
+     * Helper method to provide a single location that creates the color range
+     * filter used by this filter (in case you want to add it as a stand-alone
+     * filter).
+     *
+     * @return A new {@link ColorRange} filter for the 2013 game.
+     */
+    public static ColorRange createColorRange() {
+        int[] keepBlueMin = {0, 0, 0};
+        int[] keepBlueMax = {255, 255, 255};
+        return new ColorRange(keepBlueMin, keepBlueMax, true);
+    }
+
+    /**
+     * Helper method to provide a single location that creates the image filter
+     * used to go from a gray scale image to a black and white image.
+     *
+     * @return A image filter that converts a gray scale image to a black and
+     * white image.
+     */
+    public static BlackWhite createBlackWhite() {
+        return new BlackWhite(40, 255, false);
+    }
+
+    /**
+     * Method to filter a source image and return the filtered results.
+     *
+     * @param srcImage - The source image to be processed (passing {@code null}
+     * is not permitted).
+     *
+     * @return The original image with overlay information applied (we do a lot
+     * of filtering and try to locate the 2013 FRC rectangular target regions).
+     */
+    @Override
+    public Mat process(Mat srcImage) {
+        //Mat output = srcImage.clone();
+        Mat colorRange = _ColorRange.process(srcImage);
+        //Mat gray = _GrayScale.process(colorRange);
+        //Mat bw = _BlackWhite.process(gray);
+       // Imgproc.cvtColor(colorRange, output, Imgproc.COLOR_BW2BGR);
+	    
+        return colorRange;
+    }
+}
---
+++ src/com/techhounds/imgcv/tools/VisionTool2016.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2013, Paul Blankenbaker
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.techhounds.imgcv.tools;
+
+import com.techhounds.imgcv.FilterToolGuiOpenCv;
+import com.techhounds.imgcv.filters.CheeseButton;
+import com.techhounds.imgcv.filters.ColorRange;
+import com.techhounds.imgcv.filters.FindTarget2013;
+import com.techhounds.imgcv.filters.MatFilter;
+import com.techhounds.imgcv.filters.TTTFilter;
+import com.techhounds.imgcv.filters.VisionFilter2016;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JMenuItem;
+
+import org.opencv.core.Mat;
+import org.opencv.highgui.VideoCapture;
+
+/**
+ * A more involved example of extending the filter tool for testing filters.
+ *
+ * <p>
+ * In this second example, we exercise some more advanced features
+ * including:</p>
+ *
+ * <ul>
+ * <li>Adding preferences for some of our filters that are saved/loaded between
+ * sessions.</li>
+ * <li>Example of adding a filter implemented in-line.</li>
+ * <li>Example of adding a composite filter (multiple filters chained
+ * together).</li>
+ * </ul>
+ *
+ * @author pkb
+ */
+public final class VisionTool2016 extends FilterToolGuiOpenCv {
+
+    private VisionTool2016() { //Constructs a new instance of our example filter tool.
+        super("Vision Tool 2016");        
+    }
+
+    @Override
+    protected void addControls() { //Adding controls and filters to the side bar.
+        super.addControls(); //Adds parent controls
+        addSeparator();
+        addImageProcessingButton("2016 Filter", new VisionFilter2016()); //adds the button for our 2016 filter (VisionFilter2016.java)
+    }
+
+    /**
+     * Main entry point which allows you to run the tool as a Java Application.
+     * @param args Array of command line arguments.
+     */
+    public static void main(String[] args) {
+        // Create the GUI application and then start it's main routine
+        final FilterToolGuiOpenCv frame = new VisionTool2016();
+        frame.main();
+    }
+}
